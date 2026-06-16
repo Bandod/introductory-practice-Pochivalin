dc3d0982851c203e16c5180dd7909ab57b480f5e
Илья <ilushapochik2004@gmail.com>
2026-06-16 19:05:05 +0300

    Создан модуль 1(основы)

--- task3-python/module1_basics.py
+++ task3-python/module1_basics.py
@@ -1,1 +1,17 @@
+name = "Илья" 
+age = 18
+print(f"Привет, {name}! Тебе {age} лет.")
+
+a = 15
+b = 4
+sum_result = a + b
+diff_result = a - b
+prod_result = a * b
+quot_result = a / b
+
+print(f"Сумма {a} и {b}: {sum_result}")
+print(f"Разность {a} и {b}: {diff_result}")
+print(f"Произведение {a} и {b}: {prod_result}")
+print(f"Деление {a} и {b}: {quot_result}")
+
 
